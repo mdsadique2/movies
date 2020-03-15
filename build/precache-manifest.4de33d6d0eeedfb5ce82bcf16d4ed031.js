0fd39cbb64a5f0129eef2687122bf17f0142dcf2
Sadique <mdsadique2@gmail.com>
2020-03-15 21:10:45 +0530

    changes

---
+++ build/precache-manifest.4de33d6d0eeedfb5ce82bcf16d4ed031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d2c2514789df018d7584ba905e977d",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "e7277782dfe239c0886e",
+    "url": "/movies/static/css/2.0f300b72.chunk.css"
+  },
+  {
+    "revision": "8bf04e07bb6b039503d9",
+    "url": "/movies/static/css/main.e5b43287.chunk.css"
+  },
+  {
+    "revision": "e7277782dfe239c0886e",
+    "url": "/movies/static/js/2.dd5a3b2f.chunk.js"
+  },
+  {
+    "revision": "8f6cbdb0bd9eb6b4266517f977dcee4d",
+    "url": "/movies/static/js/2.dd5a3b2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bf04e07bb6b039503d9",
+    "url": "/movies/static/js/main.9af86923.chunk.js"
+  },
+  {
+    "revision": "714c3be4806687fd7c75",
+    "url": "/movies/static/js/runtime-main.bf370ba7.js"
+  }
+]);
