904364e6bf8601b84f2c8c6766c563e048b96d63
Sadique <mdsadique2@gmail.com>
2020-03-16 00:21:00 +0530

    Updates

---
+++ precache-manifest.1605b2b22e21149fc6dd3286ea5cdb2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e56ffaf9c933b5dfb52075bdf88ae93",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "a9a309923a69e61c927f",
+    "url": "/movies/static/css/2.0f300b72.chunk.css"
+  },
+  {
+    "revision": "beb48b6e3d5d750ab40c",
+    "url": "/movies/static/css/main.774c2d83.chunk.css"
+  },
+  {
+    "revision": "a9a309923a69e61c927f",
+    "url": "/movies/static/js/2.9eed1dce.chunk.js"
+  },
+  {
+    "revision": "8f6cbdb0bd9eb6b4266517f977dcee4d",
+    "url": "/movies/static/js/2.9eed1dce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "beb48b6e3d5d750ab40c",
+    "url": "/movies/static/js/main.b8cb9308.chunk.js"
+  },
+  {
+    "revision": "3c5a28447138d6d32ebd",
+    "url": "/movies/static/js/runtime-main.92ad4361.js"
+  }
+]);
